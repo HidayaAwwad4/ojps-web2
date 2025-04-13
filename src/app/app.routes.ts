7b015c3eb36fcb8381536422dae9e5a778678480
Hidaya Awwad <hidayaibrahimawwad4@gmail.com>
2025-04-13 18:01:21 +0300

    create job & shortlist components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { JobPostingsComponent } from './components/job-postings/job-postings.component';
 import {EmployerHomeComponent} from './components/employer-home/employer-home.component';
-
+import {ShortlistComponent} from './components/shortlist/shortlist.component';
+import {CreateJobComponent} from './components/create-job/create-job.component';
 export const routes: Routes = [
   { path: 'employer-home', component: EmployerHomeComponent },
-  { path: 'job-postings', component: JobPostingsComponent }
+  { path: 'job-postings', component: JobPostingsComponent },
+  {path: 'shortlist', component: ShortlistComponent},
+  {path: 'create-job', component: CreateJobComponent},
 ];
