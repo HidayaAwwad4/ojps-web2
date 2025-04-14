3470b8675722b4e39d7458cabee345d8b3a86240
tasneemjber <tasneemjber@gmail.com>
2025-04-14 15:27:55 +0300

    tasneem features

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,8 +7,21 @@ import {AllUsersComponent} from './components/dashboard-admin/all-users/all-user
 import {JobListingsComponent} from './components/dashboard-admin/job-listings/job-listings.component';
 import {HomepageComponent} from './components/home-page/home-page.component';
 import {ShortlistComponent} from './components/shortlist/shortlist.component';
+import {SignUpComponent} from './components/sign-up/sign-up.component';
+import {LoginComponent}from './components/login/login.component';
+import {ForgotPasswordComponent} from './components/forgot-password/forgot-password.component';
+import {VerifyCodeComponent} from './components/verify-code/verify-code.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import {ChoseUserTypeComponent} from './components/chose-user-type/chose-user-type.component';
 
 export const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'signup', component: SignUpComponent },
+  { path: 'type', component: ChoseUserTypeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'verify-code', component: VerifyCodeComponent },
+  { path: 'reset-password', component: ResetPasswordComponent },
   { path: 'home-page', component: HomepageComponent },
   { path: 'employer-home', component: EmployerHomeComponent },
   { path: 'employer/job-postings', component: JobPostingsComponent },
---
+++ src/app/components/verify-code/verify-code.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-verify-code',
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './verify-code.component.html',
+  styleUrl: './verify-code.component.css'
+})
+export class VerifyCodeComponent {
+
+}
