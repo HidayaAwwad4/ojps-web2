f5d6d2ca396d6244631a32dde92c2aa480f4d25b
tasneemjber <tasneemjber@gmail.com>
2025-04-17 13:51:42 +0300

    auth page

---
+++ src/app/components/auth/sign-up-employer/sign-up-employer.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-sign-up-employer',
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './sign-up-employer.component.html',
+  styleUrl: './sign-up-employer.component.css'
+})
+export class SignUpEmployerComponent {
+
+}
