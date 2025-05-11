0c80012c4a9e98f6bf13a9d52161295fd677664d
RazanQaddomi <151151234+RazanQaddomi@users.noreply.github.com>
2025-05-11 21:43:43 +0300

    add photos

--- frontend/src/app/components/job-seeker/job-intro/job-intro.component.ts
+++ frontend/src/app/components/job-seeker/job-intro/job-intro.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NgForOf} from '@angular/common';
 
 @Component({
@@ -10,21 +10,37 @@ import {NgForOf} from '@angular/common';
   styleUrls: ['./job-intro.component.css']
 })
 export class JobIntroComponent implements OnInit {
-  images = [
-    {
-      src: 'assets/img_1.png',
-      alt: 'Explore jobs',
-      title: 'Find Your Dream Job',
-      subtitle: 'Discover opportunities tailored just for you'
-    },
-    {
-      src: 'assets/img.png',
-      alt: 'Career path',
-      title: 'Build Your Career',
-      subtitle: 'We connect you with top employers'
-    }
-    // Add more images if you like
-  ];
+  images = [{
+    src: 'assets/web-developer.png',
+    alt: 'Web Developer',
+    title: 'Build the Future of the Web',
+    subtitle: 'Turn ideas into powerful websites.'
+  }, {
+    src: 'assets/fashion.png',
+    alt: 'Fashion',
+    title: 'Shape the World with Style',
+    subtitle: 'Inspire confidence through design.'
+  }, {
+    src: 'assets/chef.png',
+    alt: 'Chef & Cook',
+    title: 'Cook with Passion',
+    subtitle: 'Bring flavors to life with every dish.'
+  }, {
+    src: 'assets/waiter.png',
+    alt: 'Waiters',
+    title: 'Serve with Excellence',
+    subtitle: 'Create memorable dining experiences.'
+  }, {
+    src: 'assets/ui-ux.png',
+    alt: 'UI & UX Designer',
+    title: 'Design that Feels Right',
+    subtitle: 'Craft beautiful, human-centered experiences.'
+  }, {
+    src: 'assets/mobile-dev.png',
+    alt: 'Mobile Developer',
+    title: 'Power the World in Your Pocket',
+    subtitle: 'Create intuitive and smart mobile apps.'
+  }];
 
   currentIndex = 0;
 
