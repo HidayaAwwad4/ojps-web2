c6b58579a402f201c35beb986d0e970cf1addd2e
tasneemjber <tasneemjber@gmail.com>
2025-05-15 13:37:22 +0300

    finish-frontend-edit

--- frontend/src/app/components/auth/sign-up-employer/sign-up-employer.component.ts
+++ frontend/src/app/components/auth/sign-up-employer/sign-up-employer.component.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { NgIf, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-sign-up-employer',
-  imports: [
-    RouterLink
-  ],
   templateUrl: './sign-up-employer.component.html',
-  styleUrl: './sign-up-employer.component.css'
+  styleUrls: ['./sign-up-employer.component.css'],
+  standalone: true,
+  imports: [
+    FormsModule,
+    RouterLink,
+    NgIf,
+    NgClass
+  ]
 })
 export class SignUpEmployerComponent {
+  fullname: string = '';
+  companyName: string = '';
+  email: string = '';
+  password: string = '';
+  submitted: boolean = false;
+  formInvalidMessage: string = '';
 
+  constructor(private router: Router) {}
+
+  onSignUp(form: any) {
+    this.submitted = true;
+    this.formInvalidMessage = '';
+
+    if (form.invalid) {
+      this.formInvalidMessage = 'Please fill in all required fields correctly.';
+      return;
+    }
+
+
+    this.router.navigate(['/employer-home']);
+  }
 }
